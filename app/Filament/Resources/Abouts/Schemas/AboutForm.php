6787223fabd58cf6023485878ac127127b9c8de3
Aflian <afliandra2012@gmail.com>
2025-09-02 21:30:15 +0700

    Fitur About Sekolah

---
+++ app/Filament/Resources/Abouts/Schemas/AboutForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Abouts\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class AboutForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required()
+                    ->default('Tentang Sekolah'),
+                Textarea::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+}
